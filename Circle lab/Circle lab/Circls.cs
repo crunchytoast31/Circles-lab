fe4e9e9bf6e035b3cede2c04fd052b9b935f1fd8
Wail Alhashemi <72526651+crunchytoast31@users.noreply.github.com>
2022-10-11 01:47:37 -0400

    Add files via upload

---
+++ Circle lab/Circle lab/Circls.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_lab
+{
+    internal class Circles
+    {
+        public Circles(double radius)
+        {
+
+
+
+
+        }
+
+        public string CalculateCircumference(double radius)
+        {
+            double calcualted = 2 * Math.PI * radius;
+            string formattedCircumference = "The calculated circumfrence is :" + Math.Round(calcualted, 2);
+
+            return formattedCircumference;
+
+        }
+
+        public string CalculateArea(double radius)
+        {
+            double calcualted = Math.PI * radius * radius;
+            string formattedArea = "The calcualted area of your circle is: " + Math.Round(calcualted, 2);
+
+
+            return formattedArea;
+        }
+    }
+}
---
+++ Circle lab/Circle lab/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Circle_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+            {
+                i++;
+
+                Console.WriteLine("Enter Radius:");
+                double radius = 0;
+                try
+                {
+                    radius = double.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("The value you entered is not a number try again!");
+                    i--;
+                }
+
+                Circles c = new Circles(radius);
+                c.CalculateCircumference(radius);
+
+                Console.WriteLine($"Circumfrence: {c.CalculateCircumference(radius)}");
+
+                Console.WriteLine($"Area: {c.CalculateArea(radius)}");
+
+
+
+
+                if (GoOn() == false)
+                {
+                    Console.WriteLine("Number of circles :" + i);
+                    break;
+                }
+
+            }
+        }
+
+        static bool GoOn()
+        {
+
+
+            Console.WriteLine("Would you like another radius? Y/N");
+            string restart = Console.ReadLine().ToLower();
+            if (restart == "y")
+            {
+                return true;
+            }
+            else if (restart == "n")
+            {
+                Console.WriteLine("Goodbye!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Could you try again?");
+                return GoOn();
+            }
+        }
+    }
+}
